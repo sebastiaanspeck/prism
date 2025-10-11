498898b48d1fee0d2f6d9ac6dd74b97d420a9e7f
sebastiaanspeck <shem.speck@gmail.com>
2025-10-11 18:28:57 +0200

    Add support for Magik

---
+++ components/prism-magik.js
@@ -0,0 +1,78 @@
+/* eslint-disable regexp/prefer-w */
+Prism.languages.magik = {
+	'comment': [
+		{ pattern: /##.*/, greedy: true }, // documentation
+		{ pattern: /#.*/, greedy: true } // comment
+	],
+
+	'keyword': [
+		/\b_(?:class|constant|dynamic|global|import|local)\b/, // variables,
+		/\b_(?:abstract|endmethod|iter|method|private)\b/, // method
+		/\b_(?:endproc|proc)\b/, // procedure
+		/\b_(?:block|endblock)\b/, // block
+		/\b_(?:elif|else|endif|if|then)\b/, // if
+		/\b_(?:continue|endloop|finally|for|leave|loop|loopbody|over|while)\b/, // loop
+		/\b_(?:default|handling)\b/, // handling
+		/\b_(?:catch|endcatch)\b/, // catch
+		/\b_throw\b/, // throw
+		/\b_primitive\b/, // primitive
+		/\b_(?:endtry|try|when)\b/, // try
+		/\b_(?:endprotect|locking|protect|protection)\b/, // protect
+		/\b_(?:endlock|lock)\b/, // lock
+		/\b_with\b/ // standalone since _finally, _handling, _throw, _try, _leave and _continue all can have this
+	],
+
+	'builtin': [
+		/\b_(?:clone|package|self|super|thisthread|unset)\b/
+	],
+
+	'boolean': /\b_(?:false|maybe|true)\b/,
+
+	'char': {
+		pattern: /%(?:[a-zA-Z][a-zA-Z0-9_?!]*|.)/,
+		greedy: true
+	},
+
+	'variable': [
+		/\|![a-zA-Z0-9_?!]+!\|/, /\|![a-zA-Z0-9_?!]+\|!/, /!\|[a-zA-Z0-9_?!]+\|!/, /!\|\|!/, /![a-zA-Z][a-zA-Z0-9_?!]*!/, // dynamic variable
+		/[a-zA-Z_][a-zA-Z0-9_]*:[a-zA-Z_][a-zA-Z0-9_]*/, // global variable
+		/@(?:[a-zA-Z_][a-zA-Z0-9_]*:)?[a-zA-Z_][a-zA-Z0-9_]*/, // global reference
+	],
+
+	'symbol': /:(?:\|[^|]*\||[\w?!])+/,
+
+	'number': {
+		pattern: /\b\d+(?:\.\d+)?(?:[eE&][+-]?\d+)?\b|\b(?:[2-9]|[12]\d|3[0-6])[rR][a-zA-Z0-9]+\b/,
+		greedy: true
+	},
+
+	'string': {
+		pattern: /"(?:\\.|[^"\\\r\n])*"|'(?:\\.|[^'\\\r\n])*'/,
+		greedy: true
+	},
+
+	'regex': {
+		pattern: /\/(?:(?!\/)(?:\\.|[^\\\/\r\n])*\/[qisdlmuCX]*|\/)/,
+		greedy: true
+	},
+
+	'operator': [
+		/\^<</, /<</, { pattern: /_(?:and|andif|or|orif|xor)<</, greedy: true }, { pattern: /(?:\*\*\^?|\*\^?|\/\^?|_mod\^?|_div\^?|-\^?|\+\^?)<</, greedy: true }, // assignment operators
+		/>>/, /\b_return\b/, // return operators
+		/\b_(?:cf|is|isnt)\b/, /<>/, />=/, /<=/, /</, />/, /~=/, /=/, // relational operators
+		/\b_(?:and|andif|or|orif|xor)\b/, // logical operators
+		/\*\*/, /\*/, /\//, /\b_(?:div|mod)\b/, // arithmetic operators
+		/\+/, /-/, /\b_not\b/, /~/, // unary operators
+	],
+
+	'property': {
+		pattern: /_pragma.*/,
+		greedy: true,
+		inside: {
+			'modifier': /classify_level|topic|usage/,
+			'punctuation': /[={},]/
+		}
+	},
+
+	'punctuation': /[[\](){},;]/
+};
