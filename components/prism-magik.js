1596f7d5ffa3caa114f83e80f4cc49743680b9b8
Xander Petersen <56588736+xandermonk@users.noreply.github.com>
2025-10-17 14:42:33 +0200

    fix pretty much every linter issue
    the rest is fake news

--- components/prism-magik.js
+++ components/prism-magik.js
@@ -13,7 +13,7 @@ Prism.languages.magik = {
 	'declaration': [
 		{ pattern: /(_package).*/, greedy: true, lookbehind: true },
 		{ pattern: /(\b_global\s+)[^;]+/, greedy: true, lookbehind: true },
-		{ pattern: /(\b_constant\s+)[^<<]+/, greedy: true, lookbehind: true }
+		{ pattern: /(\b_constant\s+)[^<]+/, greedy: true, lookbehind: true }
 	],
 
 	'comment': [
@@ -23,7 +23,7 @@ Prism.languages.magik = {
 
 	'function': [
 		{ pattern: /\b_(?:abstract|endmethod|iter|method|private)\b/, greedy: true }, // method keywords
-		{ pattern: /\b_(?:endproc|proc)\b/ }, // procedure 
+		{ pattern: /\b_(?:endproc|proc)\b/ }, // procedure
 		{ pattern: /(\.)\s*([A-Za-z_]+)/, lookbehind: true } // method calls
 	],
 
@@ -40,23 +40,23 @@ Prism.languages.magik = {
 	],
 
 	'keyword': [
-	 	/\b_(?:class|dynamic|global|import|local)\b/i, // variables
+		/\b_(?:class|dynamic|global|import|local)\b/i, // variables
 		/\b_(?:block|endblock)\b/i, // block
 		/\b_(?:elif|else|endif|if|then)\b/i, // if
-		/\b_(?:and|andif|or|orif|xor|not)\b/i, // logical operators
+		/\b_(?:and|andif|or|orif|xor|not)\b/i, // logical operators wat is hier mis
 		/\b_(?:is|isnt)\b/i, // comparison
-	 	/\b_(?:mod|div)\b/i, // math		
+		/\b_(?:mod|div)\b/i, // math wat is hier mis
 		/\b_(?:continue|endloop|finally|for|leave|loop|loopbody|over|while)\b/i, // loop
-	 	/\b_(?:default|handling)\b/i, // handling
-	 	/\b_(?:catch|endcatch)\b/i, // catch
-	 	/\b_throw\b/i, // throw
+		/\b_(?:default|handling)\b/i, // handling
+		/\b_(?:catch|endcatch)\b/i, // catch
+		/\b_throw\b/i, // throw
 		/>>/, /\b_return\b/i, // return
-	 	/\b_primitive\b/i, // primitive
+		/\b_primitive\b/i, // primitive
 		/\b_(?:endtry|try|when)\b/i, // try
 		/\b_(?:endprotect|locking|protect|protection)\b/i, // protect
-	 	/\b_(?:endlock|lock)\b/i, // lock
-	 	/\b_with\b/i, // standalone since _finally, _handling, _throw, _try, _leave and _continue all can have this
-		/\b_(?:optional|gather|scatter|allresults)\b/i // parameter options}
+		/\b_(?:endlock|lock)\b/i, // lock
+		/\b_with\b/i, // standalone since _finally, _handling, _throw, _try, _leave and _continue all can have this
+		/\b_(?:optional|gather|scatter|allresults)\b/i // parameter options wat gaat hier mis?
 	],
 
 	'builtins': {
@@ -69,7 +69,7 @@ Prism.languages.magik = {
 	},
 
 	'operator': [
-		/\^<</, /<</, 
+		/\^<</, /<</,
 		{ pattern: /(?:\*\*\^?|\*\^?|\/\^?|-\^?|\+\^?)<</, greedy: true }, // assignment operators
 		/\b_(?:cf|is|isnt)\b/, /<>/, />=/, /<=/, /</, />/, /~=/, /=/, // relational operators
 		/\*\*/, /\*/, /\//, // arithmetic operators
@@ -83,15 +83,14 @@ Prism.languages.magik = {
 		greedy: true
 	},
 
-	// is dit nog nodig?
-	// 'variable': [
-	// 	/[a-zA-Z_][a-zA-Z0-9_]*:[a-zA-Z_][a-zA-Z0-9_]*/, // global variable
-	// 	/@(?:[a-zA-Z_][a-zA-Z0-9_]*:)?[a-zA-Z_][a-zA-Z0-9_]*/, // global reference toevoegen
-	// ],
+	'variable': [
+		/[a-zA-Z_][a-zA-Z0-9_]*:[a-zA-Z_][a-zA-Z0-9_]*/, // global variable
+		/@(?:[a-zA-Z_][a-zA-Z0-9_]*:)?[a-zA-Z_][a-zA-Z0-9_]*/, // global reference toevoegen
+	],
 
 	'symbol': {
-		 pattern: /:(?:\|[^|]*\||[\w?!])+/, 
-		 greedy: true
+		pattern: /:(?:\|[^|]*\||[\w?!])+/,
+		greedy: true
 	},
 
 	'unset': {
@@ -120,11 +119,11 @@ Prism.languages.magik = {
 	},
 
 	'variable': [
-		{ pattern: /\|![a-zA-Z0-9_?!]+!\|/}, // variable encased like |!var!|
-		{ pattern: /\|![a-zA-Z0-9_?!]+\|!/}, // variable encased like |!var|!
-		{ pattern: /!\|[a-zA-Z0-9_?!]+\|!/}, // variable encased like !|var!|
-		{ pattern: /!\|\|!/}, // empty variable !||!
-		{ pattern: /![a-zA-Z][a-zA-Z0-9_?!]*!/}, // variable encased like !var!
+		{ pattern: /\|![a-zA-Z0-9_?!]+!\|/ }, // variable encased like |!var!|
+		{ pattern: /\|![a-zA-Z0-9_?!]+\|!/ }, // variable encased like |!var|!
+		{ pattern: /!\|[a-zA-Z0-9_?!]+\|!/ }, // variable encased like !|var!|
+		{ pattern: /!\|\|!/ }, // empty variable !||!
+		{ pattern: /![a-zA-Z][a-zA-Z0-9_?!]*!/ }, // variable encased like !var!
 		{ pattern: /(^|[^.:])\b[a-zA-Z][a-zA-Z_]*\b/, lookbehind: true }
 	],
 };
