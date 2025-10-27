ca0814b985c79604910588c192e49f7e131220c2
Xander Petersen <56588736+xandermonk@users.noreply.github.com>
2025-10-27 09:50:19 +0100

    Allow method names encased in ||

--- components/prism-magik.js
+++ components/prism-magik.js
@@ -30,7 +30,8 @@ Prism.languages.magik = {
 	'function': [
 		{ pattern: /\b_(?:abstract|endmethod|iter|method|private)\b/, greedy: true }, // method keywords
 		{ pattern: /\b_(?:endproc|proc)\b/ }, // procedure
-		{ pattern: /(\.)\s*(\|?[A-Za-z_]\w*[!?]?)/, lookbehind: true }
+		{ pattern: /(\.)\s*\|[A-Za-z_]\w*[!?]?\|/, lookbehind: true },
+		{ pattern: /(\.)\s*[A-Za-z_]\w*[!?]?/, lookbehind: true }
 	],
 
 	'self': [
